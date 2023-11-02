62f09934316769edbbb345d08880909a226ace56
root <g.irakiza@alustudent.com>
2023-11-02 08:43:07 -0700

    new

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num_args = len(argv)
-    print("{:d} {:s}{:s}".format(num_args - 1, "argument" if num_args <= 2 else "arguments",
-                                 "." if num_args == 1 else ":"))
+    num = len(argv)
+    print("{:d} {:s}{:s}".format(num - 1, "argument" if num - 1 == 1 else "arguments",
+                                 "." if num - 1 == 0 else ":"))
     for i, arg in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, arg))
