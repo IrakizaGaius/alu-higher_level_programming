da1f19f715a776d45c457865f4e09225467a9c13
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-20 10:54:35 +0200

    new

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,13 +6,16 @@ class Square:
     """Represents a square
 
     Attributes:
-        __size (int): size of a side of the square
+        __size (int): size of a size of the square
+        __position (tuple): position of the square in 2D space
     """
-    def __init__(self, size=0 , position=(0, 0)):
+
+    def __init__(self, size=0, position=(0, 0)):
         """initializes the square
 
         Args:
             size (int): size of a side of the square
+            position (tuple): positoin of the square in 2D space
 
         Returns:
             None
@@ -42,7 +45,7 @@ class Square:
         """setter of __size
 
         Args:
-            value (int): the size of a size of the square
+            value (int): size of a side of the square
 
         Returns:
             None
@@ -63,28 +66,28 @@ class Square:
         """
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                print("#" * self.__size)
-        if self.__position[1] > 0:
-            for i in range(self.__position[1]):
-                print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
 
     @property
     def position(self):
         """getter of __position
 
         Returns:
-            The position of the square
+            The position of the square in 2D space
         """
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         """setter of __position
 
         Args:
-            value (int): the position of a size of the square
+            value (tuple): position of the square in 2D space
 
         Returns:
             None
@@ -94,7 +97,4 @@ class Square:
            type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            if value < 0:
-                raise TypeError("position must be a tuple of 2 positive integers")
-            else:
-                self.__position = value
+            self.__position = value
