dead6a90a370f30c9e885b615106c6b8f602d187
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-23 23:38:58 +0200

    file 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a rectangle"""
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """calculates the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """prints the rectangle description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
