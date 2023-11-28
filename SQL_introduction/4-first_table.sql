9a239f6ad1f8e4203c65864c2c3fadb98cb8e412
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-28 14:55:24 +0200

    Additional files

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+--creating  a table called first_table in the current database in MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,,
+    PRIMARY KEY (id)
+);
