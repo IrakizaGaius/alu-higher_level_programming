1b20327dbccac8c0bf7221d5b0e432c0b66659c5
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-28 17:44:22 +0200

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,11 @@
---  root user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL
-ON *.*
-TO user_0d_1@localhost;
+-- Create user_0d_1
+CREATE USER 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
