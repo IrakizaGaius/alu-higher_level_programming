8d6e9a0d850e5d8deaae7b813ebba140cf2f4a14
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-28 15:02:14 +0200

    Update 4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
---creating  a table called first_table in the current database in MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,,
-    PRIMARY KEY (id)
-);
+-- creates a table called first_table in current database
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
