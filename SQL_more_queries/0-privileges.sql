41e2fcf072a29c95c985f1314933f7773704f851
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-28 18:06:14 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,3 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2 root users
-SELECT CONCAT('Grants for ', user, '@', host) AS 'User and Host', 
-       Grant_priv, Super_priv, Create_user_priv, Process_priv, 
-       Reload_priv, Shutdown_priv, Show_db_priv, Alter_priv, 
-       Create_priv, Drop_priv, Index_priv, References_priv, 
-       Select_priv, Insert_priv, Update_priv, Delete_priv, 
-       Create_routine_priv, Alter_routine_priv, Execute_priv, 
-       Event_priv, Trigger_priv, Lock_tables_priv
-FROM mysql.user
-WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
-
--- Lists SELECT and INSERT privileges of user_0d_2 on user_2_db database
+-- Lists all privileges of the users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost' ON user_2_db;
