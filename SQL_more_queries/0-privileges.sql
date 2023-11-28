ca4a791cc4b2e2fb8c5d1c28cd69c88cf986ad1c
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2023-11-28 18:11:50 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost' ON user_2_db;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
