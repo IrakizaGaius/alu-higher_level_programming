bb3c9eaa0628aac7fc4f81c8f3c2188787cb75a3
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-02-10 21:10:52 +0200

    update on 101-scripts

--- javascript-web_jquery/101-script.js
+++ javascript-web_jquery/101-script.js
@@ -1,14 +1,9 @@
-$(document).ready(function() {
-    $('#add_item').click(function() {
-      $('ul.my_list').append('<li>Item</li>');
-    });
-  
-    $('#remove_item').click(function() {
-      $('ul.my_list li:last-child').remove();
-    });
-  
-    $('#clear_list').click(function() {
-      $('ul.my_list').empty();
-    });
+$('#add_item').click(function() {
+    $('ul.my_list').append('<li>Item</li>');
   });
-  
+$('#remove_item').click(function() {
+    $('ul.my_list li:last-child').remove();
+  });
+$('#clear_list').click(function() {
+    $('ul.my_list').empty();
+  });
